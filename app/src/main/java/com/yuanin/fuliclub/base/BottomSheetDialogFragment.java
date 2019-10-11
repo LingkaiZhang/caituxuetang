3a04062723e8e10f5052f7a4366499d7c0f1a025
lingkai <lingkai2313@163.com>
2019-10-11 13:32:56 +0800

    购买订单详情页面

---
+++ app/src/main/java/com/yuanin/fuliclub/base/BottomSheetDialogFragment.java
@@ -0,0 +1,20 @@
+package com.yuanin.fuliclub.base;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatDialogFragment;
+
+/**
+ * Modal bottom sheet. This is a version of {@link DialogFragment} that shows a bottom sheet
+ * using {@link BottomSheetDialog} instead of a floating dialog.
+ */
+public class BottomSheetDialogFragment extends AppCompatDialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new BottomSheetDialog(getContext(), getTheme());
+    }
+
+}
